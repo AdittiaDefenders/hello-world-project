05e5455d8b2a1fd42dc2af74dd7560cb1e689e9a
AdittiaDefenders <pemaubotak@gmail.com>
2025-05-27 20:06:02 +0700

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# Program Hello World sederhana
+# Dibuat oleh: [Adittia Sulistio]
+# Tanggal: [27 may 2025]
+
+def main():
+    """
+    Fungsi utama untuk menampilkan pesan Hello World
+    """
+    print("Hello, World!")
+    print("Selamat datang di GitHub!")
+    print("Ini adalah project Matakuliah saya.")
+
+if __name__ == "__main__":
+    main()
